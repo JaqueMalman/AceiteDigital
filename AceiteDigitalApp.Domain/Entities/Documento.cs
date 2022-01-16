40e8c20769181a52d76cd67cb7ca69797865eac3
Jaqueline M. Nunes <jaque.malman@gmail.com>
2022-01-15 21:28:33 -0300

    correção readme

---
+++ AceiteDigitalApp.Domain/Entities/Documento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AceiteDigitalApp.Domain.Entities
+{
+    public class Documento : BaseEntity
+    {
+        private Documento()
+        { 
+            //Necessário para entity framework
+        }
+
+        public string Titulo { get; private set; }
+
+        public string Descricao { get; private set; }
+
+        public DateTime DataCriacao { get; private set; }
+
+        private readonly List<Evento> _eventos = new ();
+        public IReadOnlyCollection<Evento> Eventos => 
+            _eventos.AsReadOnly();
+
+        private readonly List<DocumentoSignatario> _documentosSignatarios = new ();
+        public IReadOnlyCollection<DocumentoSignatario> DocumentosSignatarios =>
+            _documentosSignatarios.AsReadOnly();
+
+    }
+}
---
+++ AceiteDigitalApp.Domain/Entities/Signatario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AceiteDigitalApp.Domain.Entities
+{
+    public class Signatario : BaseEntity
+    {
+        private Signatario()
+        { 
+            //utilizado pelo EF
+        }
+
+        public string Cpf { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public string Email { get; private set; }
+
+        private readonly List<DocumentoSignatario> _documentosSignatarios = new();
+        public IReadOnlyCollection<DocumentoSignatario> DocumentosSignatarios =>
+            _documentosSignatarios.AsReadOnly();
+    }
+}
